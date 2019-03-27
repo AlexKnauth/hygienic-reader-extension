1ae4d6438fabecead7aab65682ae4876584db8d8
AlexKnauth <alexander@knauth.org>
2019-03-27 18:56:24 -0400

    only use syntax-local-identifier-as-binding if it exists

--- hygienic-reader-extension/tests/hygienically-introduce-unhygienic-macro/unhygienic-macro.rkt
+++ hygienic-reader-extension/tests/hygienically-introduce-unhygienic-macro/unhygienic-macro.rkt
@@ -5,6 +5,12 @@
 (require syntax/parse/define
          (for-syntax racket/base))
 
+(begin-for-syntax
+  (define syntax-local-identifier-as-binding
+    (dynamic-require 'racket/base
+                     'syntax-local-identifier-as-binding
+                     (λ () (λ (x) x)))))
+
 (define-simple-macro (define-it)
   #:with x (syntax-local-identifier-as-binding
             (syntax-local-introduce #'it))
